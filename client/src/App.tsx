e9a837a01536340a422b6de2a7ff82a0da77648f
Dan Flatt <108902970+Flattorama@users.noreply.github.com>
2025-12-27 09:03:31 -0500

    Wrap AppRoutes with Router for base URL
    Added Router wrapper to AppRoutes for base URL handling.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, Router } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -8,10 +8,14 @@ import NotFound from "@/pages/not-found";
 
 function AppRoutes() {
   return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route component={NotFound} />
-    </Switch>
+    // We add the Router wrapper here with the base prop.
+    // This tells wouter to ignore "/UJwebsite" in the URL.
+    <Router base="/UJwebsite">
+      <Switch>
+        <Route path="/" component={Home} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
   );
 }
 
